5e9c2725c8e279dc3a6c0f42d006ce4b3ba0b670
HampCode <pcsiheme@upc.edu.pe>
2024-06-19 10:45:07 -0500

    update apiurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment={
-    producction: false,
-    apiUrl: 'http://localhost:3000/api/v1'
-    //apiUrl: 'https://edteam-api.onrender.com/api/v1'
+    producction: false,    
+    apiUrl: 'https://edteam-api.onrender.com/api/v1'
 }
